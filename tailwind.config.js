6e08daf9f35e9271647ea31f217b4e92ba2a243e
osvaldecir viana <manocatanduva@gmail.com>
2024-07-30 16:32:20 -0300

    //

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
+      fontFamily:{
+        'sans':['Poppins', 'sans-serif']
+      },
       backgroundImage:{
         "home": "url('/assets/bg.png')"
       }
