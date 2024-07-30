b3683f6db3b36abce67a9be72b9b0f4e8a4ee799
osvaldecir viana <manocatanduva@gmail.com>
2024-07-30 18:49:08 -0300

    //

---
+++ script.js
@@ -0,0 +1,12 @@
+const menu = document.getElementById("menu")
+const cartBtn = document.getElementById("cart-btn")
+const cartModal = document.getElementById("cart-modal")
+const castItemContainer = document.getElementById("cart-items")
+const cartToral = document.getElementById("cart-total")
+const checkoutBtn = document.getElementById("checkout-btn")
+const closeModalBtn = document.getElementById("close-modal-btn")
+const cartConter = document.getElementById("cart-count")
+const addressImput = document.getElementById("address")
+const addressWarn = document.getElementById("address-warn")
+
+
