38266215ecf41db1450c24637d1346c94b5d2698
osvaldecir viana <manocatanduva@gmail.com>
2024-07-31 13:55:59 -0300

    //

--- script.js
+++ script.js
@@ -9,4 +9,52 @@ const cartConter = document.getElementById("cart-count")
 const addressImput = document.getElementById("address")
 const addressWarn = document.getElementById("address-warn")
 
+let cart = []
 
+cartBtn.addEventListener("click", function(){
+    cartModal.style.display = "flex"
+});
+
+cartModal.addEventListener("click",function(event){
+    if(event.target === cartModal){
+        cartModal.style.display = "none"
+    }
+});
+
+closeModalBtn.addEventListener("click", function(){
+    cartModal.style.display = "none"
+})
+
+menu.addEventListener("click", function(event){
+    let parentButton = event.target.closest(".add-to-cart-btn");
+    if(parentButton){
+        const name = parentButton.getAttribute("data-name")
+        const price = parseFloat(parentButton.getAttribute("data-price"))
+        
+        addToCart(name,price)
+    }
+})
+
+function addToCart(name, price){
+    const existinItem = cart.find(item => item === name)
+    if (existinItem){
+        existinItem.quantity += 1;
+ 
+    
+}else{}
+cart.push({
+    name,
+    price,
+    quantity: 1,
+})
+
+updatCartModal1(){
+
+
+}
+function updatCartModal1(){
+
+}
+
+
+}
